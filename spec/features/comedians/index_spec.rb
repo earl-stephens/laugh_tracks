5ba34dc799d750fd073906bd8669fc395f338bd0
earl-stephens <stephensearl@hotmail.com>
2019-03-13 08:24:45 -0600

    started on statistics feature test and made a model test for avg run length

--- spec/features/comedians/index_spec.rb
+++ spec/features/comedians/index_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 describe "comedian index page" do
   before :each do
     @comedian_1 = create(:comedian)
-    @comedian_2 = create(:comedian)
-    @comedian_3 = create(:comedian)
+    @comedian_2 = create(:comedian, age: 30, city: "Seattle")
+    @comedian_3 = create(:comedian, age: 35, city: "San Diego")
     @special_1 = create(:special, comedian_id: @comedian_1.id)
-    @special_2 = create(:special, comedian_id: @comedian_1.id)
-    @special_3 = create(:special, comedian_id: @comedian_2.id)
-    @special_4 = create(:special, comedian_id: @comedian_2.id)
-    @special_5 = create(:special, comedian_id: @comedian_3.id)
+    @special_2 = create(:special, comedian_id: @comedian_1.id, length: 55)
+    @special_3 = create(:special, comedian_id: @comedian_2.id, length: 65)
+    @special_4 = create(:special, comedian_id: @comedian_2.id, length: 65)
+    @special_5 = create(:special, comedian_id: @comedian_3.id, length: 60)
   end
 
   it "shows list of comedians" do
@@ -32,7 +32,7 @@ describe "comedian index page" do
 
   it "shows list of comedian specials" do
     visit root_path
-save_and_open_page
+# save_and_open_page
     within ".comedian#{@comedian_1.id}"do
     # binding.pry
       expect(page).to have_content(@comedian_1.name)
@@ -60,4 +60,17 @@ save_and_open_page
       expect(page).to have_content(@special_4.length)
     end
   end
+
+  it "shows a statistics area" do
+    visit root_path
+
+    expected = ["Portland", "Seattle", "San Diego"]
+
+    within ".statistics" do
+      expect(page).to have_content("Average age: 25")
+      expect(page).to have_content("Average special run length: 60")
+      expect(page).to have_content("Cities: #{expected}")
+    end
+  end
+
 end
--- spec/models/special_spec.rb
+++ spec/models/special_spec.rb
@@ -10,4 +10,21 @@ RSpec.describe Special, type: :model do
     it { should validate_presence_of :title }
     it { should validate_presence_of :length }
   end
+
+  describe 'class methods' do
+    before :each do
+      @comedian_1 = create(:comedian)
+      @comedian_2 = create(:comedian, age: 30, city: "Seattle")
+      @comedian_3 = create(:comedian, age: 35, city: "San Diego")
+      @special_1 = create(:special, comedian_id: @comedian_1.id)
+      @special_2 = create(:special, comedian_id: @comedian_1.id, length: 55)
+      @special_3 = create(:special, comedian_id: @comedian_2.id, length: 65)
+      @special_4 = create(:special, comedian_id: @comedian_2.id, length: 65)
+      @special_5 = create(:special, comedian_id: @comedian_3.id, length: 60)
+    end
+
+    it ".avg_run_length" do
+      expect(Special.avg_run_length).to eq(60)
+    end
+  end
 end
